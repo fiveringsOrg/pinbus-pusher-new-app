2aa6b38608941f33ce511832b0283b124fa9f44b
phattrandev <phat.tran@pinbus.vn>
2023-06-12 17:45:21 +0700

    fix msg, route private

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,40 @@
 import React from "react";
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import { Login } from "./pages/Login.page";
 import { DeployCabinet } from "./pages/operate/DeployCabinet.page";
 import { Operate } from "./pages/operate/Operate.page";
 import { RecycleCabinet } from "./pages/operate/RecycleCabinet.page";
 import { RecyclePowerbank } from "./pages/operate/RecyclePowerbank.page";
 import { getToken } from "./utils/storage.util";
-
 export default function Router() {
+  const location = useLocation();
   const [isLogin, setIsLogin] = React.useState(false);
 
   React.useEffect(() => {
     const getCurrentToken = getToken();
     if (getCurrentToken) {
       setIsLogin(true);
+    } else {
+      setIsLogin(false);
     }
-  }, []);
+  }, [location.pathname]);
+
   let element = useRoutes([
     { path: "/", element: isLogin ? <Operate /> : <Login /> },
     { path: "/login", element: <Login /> },
-    { path: "/operate", element: <Operate /> },
-    { path: "/operate/deploy-cabinet", element: <DeployCabinet /> },
-    { path: "/operate/recycle-cabinet", element: <RecycleCabinet /> },
-    { path: "/operate/recycle-powerbank", element: <RecyclePowerbank /> },
+    { path: "/operate", element: isLogin ? <Operate /> : <Login /> },
+    {
+      path: "/operate/deploy-cabinet",
+      element: isLogin ? <DeployCabinet /> : <Login />,
+    },
+    {
+      path: "/operate/recycle-cabinet",
+      element: isLogin ? <RecycleCabinet /> : <Login />,
+    },
+    {
+      path: "/operate/recycle-powerbank",
+      element: isLogin ? <RecyclePowerbank /> : <Login />,
+    },
     { path: "*", element: <Operate /> },
   ]);
 
